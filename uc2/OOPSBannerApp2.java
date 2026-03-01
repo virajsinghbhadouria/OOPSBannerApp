2ffcaecd6dd697ece163d1880703f873e1a1d2d2
unknown <vs9771@srmist.edu.in>
2026-03-01 22:10:57 +0530

    Coded UC2 Render OOPS as Banner

---
+++ uc2/OOPSBannerApp2.java
@@ -0,0 +1,19 @@
+/**
+ * OOPSBannerApp UC2 – Render OOPS as Banner
+ *
+ * Displays the OOPS acronym in a 7x9 stylized character banner.
+ *
+ * @author Developer
+ * @version 2.0
+ */
+public class OOPSBannerApp2 {
+    public static void main(String[] args) {
+        System.out.println(" *** " + "   " + " *** " + "   " + "***** " + "   " + " **** ");
+        System.out.println("** **" + "   " + "** **" + "   " + "** **" + "   " + "** ");
+        System.out.println("** **" + "   " + "** **" + "   " + "** **" + "   " + "** ");
+        System.out.println("** **" + "   " + "** **" + "   " + "***** " + "   " + " *** ");
+        System.out.println("** **" + "   " + "** **" + "   " + "** " + "   " + "    **");
+        System.out.println("** **" + "   " + "** **" + "   " + "** " + "   " + "    **");
+        System.out.println(" *** " + "   " + " *** " + "   " + "** " + "   " + " **** ");
+    }
+}
